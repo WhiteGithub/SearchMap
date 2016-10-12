a4615ada888077904b44fb2b0b38b2213b416d1f
yu ni <yuni@yus-imac.local>
2016-10-11 20:10:54 -0400

    Initial Commit

---
+++ SearchMap Ottawa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchMap Ottawa
+//
+//  Created by yu ni on 2016-10-11.
+//  Copyright © 2016 yu ni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
